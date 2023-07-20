0e1705888d06b2471bb4ba75d9b08c81021c4757
Ibrahim <110994882+ebrahimosah@users.noreply.github.com>
2023-07-20 09:23:39 +0100

    mathematical operations buttons functional && color-scheme responsive

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const colorThemes = document.querySelectorAll('[name="theme')
+const colorThemes = document.querySelectorAll('[name="theme');
+const numbers = document.querySelectorAll('button');
+const screen = document.querySelector('.screen');
 
 const storeTheme = (theme)=>{
     localStorage.setItem('theme', theme);
@@ -18,5 +20,63 @@ colorThemes.forEach(themeOption => {
         storeTheme(themeOption.id);
     })
 })
+
+
+let firstNumber = '';
+let secondNumber = '';
+
+let operator = '';
+
+function addNumber(number) {
+    if (operator === ''){
+        firstNumber += number;
+        screen.innerText = firstNumber;
+    } else{
+        secondNumber += number;
+        screen.innerText = secondNumber;
+       
+    }
+};
+
+function addSymbol(symbol) {
+    operator = symbol;
+    screen.innerText='';
+    // firstNumber = '';
+    // secondNumber='';
+}
+
+
+function calculate(){
+    if (firstNumber !== '' &&  operator !== '' && secondNumber !== ''){
+        // let secondNumber = screen.innerText;
+        try{
+            const result = math.evaluate(`${firstNumber} ${operator} ${secondNumber}`)
+            console.log(result);
+            screen.innerText = result;
+            firstNumber = result.toString();
+            secondNumber='';
+            operator = '';
+        } catch(error){
+            console.log(error);
+        }
+    }
+}
+
+function clearResult(){
+    firstNumber = '';
+    secondNumber='';
+    operator = '';
+    screen.innerText = '';
+}
+
+
+
+
+
+
+
+
+
+
 
 document.onload = retriveTheme();
